665e72264b2122a209ae113777a1262fd37095ee
Elise Yohm <elisery@hotmail.com>
2018-03-19 21:39:15 -0700

    Work on intro and token selection screens

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  /*
+  Functions:
+  1. square clicked
+  2. choose square for computer to click
+  3. check if square is empty or occupied
+  4. check for winner
+  5. check for tie
+  6. reset Functions
+  7. set scoreboard
+  8. hide board, show intro
+  9. on click of one-player, hide intro, show board
+  */
+  let board = [' ', ' ', ' ',
+               ' ', ' ', ' ',
+               ' ', ' ', ' '
+              ];
+  let winningCombos = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ];
+
+  //PLAY AREA & TOKEN SCREEN HIDE
+  //PLAY AREA & INTRO hide
+  //INTRO & TOKEN HIDE 
+
+
+
+}); //document ready
